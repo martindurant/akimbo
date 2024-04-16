9e716438c91af2eef5dfabf6432e1a5a033f5d70
Martin Durant <martin.durant@alumni.utoronto.ca>
2024-04-16 10:57:31 -0400

    Add shonky test

--- tests/test_base.py
+++ tests/test_base.py
@@ -27,3 +27,17 @@ def test_astype_to_ak():
     assert s2.tolist() == [[6, 2, 3], [4, 5]]
     assert (s2 == s).tolist() == [[True, True, True], [True, True]]
     assert (s2 == s).all()
+
+
+@pytest.mark.skipif(
+    pd.__version__.split(".") < ["2", "2"], reason="Explode not dispatched"
+)
+def test_explode_basic():
+    data = [None, [], [1, 1, 2]]
+    s = pd.Series(data, dtype=object)
+    s2 = s.astype("awkward")
+    o = s.explode()
+    o2 = s2.explode()
+    assert o2.dtype == "awkward"
+    assert (o.isna() == o2.isna()).all()
+    assert o[~o.isna()].tolist() == o2[~o.isna()].tolist()
